daa73a457f88633eb47d583cd4aa6751a2e81323
mszydlowski1 <226408@student.pwr.edu.pl>
2018-03-08 00:56:35 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Cześć Mateusz, słyszałem, ze uczysz sie programowania.");
+        System.out.println(true);
+        System.out.println("Czy twoje inicjały to MS?");
+        System.out.print("Nie,");
+        System.out.println(false);
+
+
+
+    }
+}
