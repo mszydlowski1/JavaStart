f4c051bb83a637a7f4951890b0a8ce775edf8919
mszydlowski1 <226408@student.pwr.edu.pl>
2018-03-09 14:08:41 +0100

    nauka

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,13 @@ public class Main {
         result = x > y;
         System.out.println(result);
         System.out.print("Czy x pomnozone przez 2 jest wieksze od y?");
-        result = x > y;
+        result = 2*x > y;
         System.out.println(result);
-        System.out.print("Czy x jest wieksze od y?");
-        result = x > y;
+        System.out.print("Czy y jest mniejsze od sumy  x+3 i jednoczesnie wieksze od x pomniejszonego o 2?");
+        result = x+3 > y && y > x-2;
         System.out.println(result);
-        System.out.print("Czy x jest wieksze od y?");
-        result = x > y;
+        System.out.print("Czy iloczyn x i y jest parzysty?");
+        result = (x*y%2)==0;
         System.out.println(result);
 
 
